b9f60d422b71a8940d50749158dad598e50c66b8
HOIY <2249692487@users.noreply.github.com>
2019-07-03 18:09:23 +0800

    字典controller

---
+++ src/main/java/com/qln/workreserve/dbo/Dictionary.java
@@ -0,0 +1,31 @@
+package com.qln.workreserve.dbo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @Title: $
+ * @Project: csai-api
+ * @Description: TODO
+ * @Author 秦莉娜
+ * @Date 2019/7/3$ 9:35$
+ * @Version V2.0
+ **/
+@Getter
+@Setter
+@Entity
+@Table(name = "qln_dictionary_clc_category")
+public class Dictionary {
+    @Id
+    @GenericGenerator(name = "id", strategy = "uuid")
+    private String id;
+    private String code;
+    private String name;
+    private String parentId;
+    private boolean root;
+}
