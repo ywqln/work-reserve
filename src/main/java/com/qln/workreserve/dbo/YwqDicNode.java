3625a0c8540fede0c229a8424a0980eff9a4332b
燕文强 <yanwq@centaline.com.cn>
2019-07-04 19:39:16 +0800

    add Dictionary deal

---
+++ src/main/java/com/qln/workreserve/dbo/YwqDicNode.java
@@ -0,0 +1,28 @@
+package com.qln.workreserve.dbo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * 描述:待描述.
+ * <p>
+ *
+ * @author yanwenqiang.
+ * @date 2019/7/4
+ */
+@Entity
+@Getter
+@Setter
+@Table(name = "dictionary")
+public class YwqDicNode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String dicId;
+    private String code;
+    private String name;
+    private String parentId;
+    private boolean root;
+}
---
+++ src/main/java/com/qln/workreserve/repository/YwqDicNodeRepository.java
@@ -0,0 +1,14 @@
+package com.qln.workreserve.repository;
+
+import com.qln.workreserve.dbo.YwqDicNode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 描述:待描述.
+ * <p>
+ *
+ * @author yanwenqiang.
+ * @date 2019/7/4
+ */
+public interface YwqDicNodeRepository extends JpaRepository<YwqDicNode, Integer> {
+}
